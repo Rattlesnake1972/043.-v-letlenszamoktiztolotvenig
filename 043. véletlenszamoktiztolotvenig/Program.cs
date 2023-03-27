23f3fc9627c170cbd33f5d025f082b7369d62b3c
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-27 19:39:59 +0200

    10-től 50-ig véletlen számokat generál

--- 043. véletlenszamoktiztolotvenig/Program.cs
+++ 043. véletlenszamoktiztolotvenig/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _043.véletlenszamoktiztolotvenig
+namespace _043.veletlenszamoktiztolotvenig
 {
     class Program
     {
